49f143f82c332c60bff8fb33e39c68dbfa199d6f
__saito__ <thetakahito@gmail.com>
2020-05-21 21:39:58 +0900

    Improve: Define and apply exception class

--- lightnovel/core.py
+++ lightnovel/core.py
@@ -40,11 +40,89 @@ class Novelizer(object):
         Materialize morpheme model as DB
         """
 
-        self.model.save(True)
+        try:
+            if not self.model:
+                raise ModelNotFoundError("build")
+        except NovelizerError as e:
+            print(e)
+        else:
+            self.model.save(True)
 
     def inspect(self) -> None:
         """
         Validate the formed morpheme model
         """
 
-        self.model.inspect()
+        try:
+            if not self.model:
+                raise ModelNotFoundError("inspect")
+        except NovelizerError as e:
+            print(e)
+        else:
+            self.model.inspect()
+
+
+class NovelizerError(Exception):
+    """
+    Exception base for Novelizer class
+
+    Attributes
+    ----------
+    name : str
+        Error name
+    """
+
+    def __init__(self) -> None:
+        self.name = self.__class__.__name__
+
+    def msg(self, text: str) -> str:
+        """
+        Error message
+
+        Parameters
+        ----------
+        text : str
+            Error text
+
+        Returns
+        -------
+        err_msg : str
+            An error message containing the error name and its contents
+        """
+
+        return f"{ self.name }: { text }"
+
+
+class ModelNotFoundError(NovelizerError):
+    """
+    Error thrown when there is no morpheme model
+
+    Attributes
+    ----------
+    type : str
+        The thrown function name
+    """
+
+    def __init__(self, type: str) -> None:
+        """
+        Parameters
+        ----------
+        type : str
+            The thrown function name
+        """
+
+        super().__init__()
+        self.type = type
+
+    def __str__(self) -> str:
+        """
+        Returns
+        -------
+        err_msg : str
+            An error message containing the error name and its contents
+        """
+
+        return super().msg(
+            f"Could not execute { self.type } function because "
+            "there is no morpheme model. First, "
+            "create the model using the register function.")
